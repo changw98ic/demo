4ca774b8f15306a890dd524387d86ba89e749912
chang <changw98@126.com>
2020-03-06 10:43:31 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/baseController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class baseController {
+    @RequestMapping("/get")
+    @ResponseBody
+    public String num1()
+    {
+        return "HelloWorld";
+    }
+}
